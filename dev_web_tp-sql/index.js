da1bc643861f3bdbbef7a893614cda9efa5ce6c9
DavidFarag <159547239+DavidFarag@users.noreply.github.com>
2025-01-12 13:51:45 +0100

    Add files via upload

---
+++ dev_web_tp-sql/index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const app = express();
+const port = 3000;
+const usersRouter = require("./routes/users.js")
+const sqlite3 = require("sqlite3").verbose()
+
+//MIDDLEWARE
+app.use(express.json());
+//USER Endpoint
+app.use("/api/", usersRouter)
+
+// GET : HOME GET METHOD
+app.get("/", (req, res) => {
+    res.json({
+        msg: "Welcome to my users API",
+    })
+})
+
+app.listen(port, () => {
+	console.log(`Serveur en cours d'exécution sur http://localhost:${port}`)
+})
